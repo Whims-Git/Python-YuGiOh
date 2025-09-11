6786e70dab19d994bd4369d081ca72676834218b
Whims-Git <andylegma2000@gmail.com>
2025-09-11 01:06:42 -0400

    Created Extra Deck Dictionary

---
+++ extra_deck_monster.py
@@ -0,0 +1,16 @@
+# Yu-Gi-Oh Extra Deck Monster Card Database
+# Each card is represented as a dictionary with 8 characteristics.
+
+extra_deck_monster_cards = [
+    {
+        "Name": "Charubin the Fire Knight",
+        "Attribute": "FIRE",
+        "Level": 3,
+        "Type": "Pyro, Fusion, Normal",
+        "Materials": "Monster Egg + Hinotama Soul",
+        "Effect": "None",
+        "ATK": 1100,
+        "DEF": 800
+    }
+    # Add more cards below using the same format
+]
